f2b9e041e4ad286af35c2977b77cc84e3f81a37a
Christian Ducros <96232175+christianDUCROS@users.noreply.github.com>
2023-04-28 17:33:40 +0200

    Update servo2.py

--- servo2.py
+++ servo2.py
@@ -2,11 +2,11 @@
 from machine import Pin, PWM , ADC
 import time 
 servo0 = PWM(Pin(0))      # create PWM object from a pin
-servo0.freq(20)         # set frequency
+servo0.freq(50)         # set frequency
 
 adc = ADC(Pin(26))     # create ADC object on ADC pin
 
 while True :
     valeur_pot = adc.read_u16()    # read value, 0-65535 
-    servo0.duty_u16(int(2300*valeur_pot/65535 + 800))    #duty_cycle min = 800 duty_cycle_max = 3100
+    servo0.duty_u16(int(5500*valeur_pot/65535 + 2000))    #duty_cycle min = 800 duty_cycle_max = 3100
     time.sleep_ms(20)
