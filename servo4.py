d722f2a4710beb3d8f651fbc574e466d127db036
Christian Ducros <96232175+christianDUCROS@users.noreply.github.com>
2023-04-28 17:34:41 +0200

    Update servo4.py

--- servo4.py
+++ servo4.py
@@ -3,7 +3,7 @@
 from machine import Pin, PWM , ADC
 import time 
 servo0 = PWM(Pin(0))      # create PWM object from a pin
-servo0.freq(20)         # set frequency
+servo0.freq(50)         # set frequency
 
 adc = ADC(Pin(26))     # create ADC object on ADC pin
 
@@ -12,7 +12,7 @@ def map_range(x, in_min, in_max, out_min, out_max):
 
 
 valeur_angle = 30    
-val_duty_cycle = map_range(valeur_angle,-90,90,800,3100)  #duty_cycle min = 800 duty_cycle_max = 3100
+val_duty_cycle = map_range(valeur_angle,-90,90,2000,7500)  #duty_cycle min = 800 duty_cycle_max = 3100
 servo0.duty_u16(val_duty_cycle)     
 
 
