d9c5ea10e136d7567b3047f8ee77f05e4bcd33b4
Christian Ducros <96232175+christianDUCROS@users.noreply.github.com>
2023-04-28 17:33:11 +0200

    Update servo1.py

--- servo1.py
+++ servo1.py
@@ -2,12 +2,12 @@
 from machine import Pin, PWM
 import time 
 pwm0 = PWM(Pin(0))      # create PWM object from a pin
-pwm0.freq(20)         # set frequency
+pwm0.freq(50)         # set frequency
 
 while True : 
-    pwm0.duty_u16(3100)         # get current duty cycle, range 0-65535
+    pwm0.duty_u16(7500)         # get current duty cycle, range 0-65535
     time.sleep(4)           
-    pwm0.duty_u16(800)         # get current duty cycle, range 0-65535
+    pwm0.duty_u16(2000)         # get current duty cycle, range 0-65535
     time.sleep(4)        
 
 
@@ -16,11 +16,11 @@ while True :
 from machine import Pin, PWM
 import time 
 pwm0 = PWM(Pin(15))      # create PWM object from a pin 15
-pwm0.freq(20)                   # set frequency
+pwm0.freq(50)                   # set frequency
 
 while True :
-    pwm0.duty_u16(3100)
+    pwm0.duty_u16(7500)
     time.sleep(4)           
-    pwm0.duty_u16(800)
+    pwm0.duty_u16(2000)
     time.sleep(4)        
 '''
