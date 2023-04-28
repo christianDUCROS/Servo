d6ef91e987cea0b45c6fad7250810b01bda39316
Christian Ducros <96232175+christianDUCROS@users.noreply.github.com>
2023-04-28 17:35:20 +0200

    Update servo5.py

--- servo5.py
+++ servo5.py
@@ -1,7 +1,7 @@
 #RP2 + objet servo 
 import servo   #duty_cycle min = 800 duty_cycle_max = 3100
-servo0 = servo.servo(0,0,180,800,3100)      # create servo  : broche0 vitesse par defaut =0 
-servo1 = servo.servo(1,0,180,800,3100,100)      # create servo  : broche1 vitesse = 100 
+servo0 = servo.servo(0,0,180,2000,7500)      # create servo  : broche0 vitesse par defaut =0 
+servo1 = servo.servo(1,0,180,2000,7500,100)      # create servo  : broche1 vitesse = 100 
 
 valeur_angle = 150     
 servo0.rotation(valeur_angle)
